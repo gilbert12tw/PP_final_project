3aec261d234e52ca7cebb2e34e3ec514a61d34e4
Jameson <jiehongchen726@gmail.com>
2024-12-13 20:36:32 +0800

    mckp extra avg 0.655s

--- knapsack_mckp.cu
+++ knapsack_mckp.cu
@@ -1,8 +1,8 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include <algorithm>
 #include <cuda.h>
 #include <cuda_runtime.h>
-#include <algorithm>
+#include <stdio.h>
+#include <stdlib.h>
 
 #define BLOCK_SIZE 1024
 #define max(a, b) (a > b ? a : b)
@@ -12,41 +12,38 @@ struct Item {
     int weight, value;
 };
 
-bool compareByValue(const Item& a, const Item& b) {
+__forceinline__ bool compareByValue(const Item &a, const Item &b) {
     if (a.value == b.value) {
-        return a.weight < b.weight;  
+        return a.weight < b.weight;
     }
     return a.value < b.value;
 }
+__forceinline__ int ceil_div(int a, int b) { return (a + b - 1) / b; }
 
-__global__ void mckp_kernel(int* dp_prev, int* dp_curr, int m, 
-                           int* group_weights, int group_size, int group_value) {
+__global__ void mckp_kernel(int *dp_prev, int *dp_curr,
+                            int *group_weights, int group_value) {
     int idx = blockIdx.x * blockDim.x + threadIdx.x;
-    
+
     __shared__ int shared_weights[BLOCK_SIZE];
-    
-    if (threadIdx.x < group_size) {
-        shared_weights[threadIdx.x] = group_weights[threadIdx.x];
-    }
+
+    shared_weights[threadIdx.x] = group_weights[threadIdx.x];
     __syncthreads();
-    
-    if (idx <= m) {
-        int maxVal = dp_prev[idx];  
-        int sumW = 0;
-        
-        for (int k = 0; k < min(group_size, BLOCK_SIZE); k++) {
-            sumW += shared_weights[k];
-            if (sumW > idx) break;  
-            
-            maxVal = max(maxVal, dp_prev[idx - sumW] + group_value * (k + 1));
-        }
-        
-        dp_curr[idx] = maxVal;
+
+    int maxVal = dp_prev[idx];
+    int sumW = 0;
+
+    for (int k = 0; k < BLOCK_SIZE; k++) {
+        sumW += shared_weights[k];
+        if (sumW > idx) break;
+
+        maxVal = max(maxVal, dp_prev[idx - sumW] + group_value * (k + 1));
     }
+
+    dp_curr[idx] = maxVal;
 }
 
-void input(char* infile, int& n, int& m, Item*& items) {
-    FILE* file = fopen(infile, "rb");
+void input(char *infile, int &n, int &m, Item *&items) {
+    FILE *file = fopen(infile, "rb");
     if (!file) {
         printf("Error: Cannot open input file\n");
         exit(1);
@@ -54,37 +51,41 @@ void input(char* infile, int& n, int& m, Item*& items) {
 
     fread(&n, sizeof(int), 1, file);
     fread(&m, sizeof(int), 1, file);
-    
-    items = (Item*)malloc(n * sizeof(Item));
-    
+
+    items = (Item *)malloc(n * sizeof(Item));
+
     fread(items, sizeof(int), 2 * n, file);
-    
+
     fclose(file);
 }
 
-void processGroups(Item* items, int n, int*& group_counts, int*& unique_values, int& num_groups) {
+void processGroups(Item *items, int n, int *&group_counts, int *&unique_values, int &num_groups) {
     std::sort(items, items + n, compareByValue);
-    
+
     num_groups = 1;
     int curr_value = items[0].value;
+    int g_cnt = 1;
     for (int i = 1; i < n; i++) {
-        if (items[i].value != curr_value) {
+        if (items[i].value != curr_value || g_cnt >= BLOCK_SIZE) {
             num_groups++;
             curr_value = items[i].value;
+            g_cnt = 1;
+        } else {
+            ++g_cnt;
         }
     }
-    
-    group_counts = (int*)malloc(num_groups * sizeof(int));
-    unique_values = (int*)malloc(num_groups * sizeof(int));
+
+    group_counts = (int *)malloc(num_groups * sizeof(int));
+    unique_values = (int *)malloc(num_groups * sizeof(int));
     memset(group_counts, 0, num_groups * sizeof(int));
-    
+
     int group_idx = 0;
     curr_value = items[0].value;
     group_counts[0] = 1;
     unique_values[0] = curr_value;
-    
+
     for (int i = 1; i < n; i++) {
-        if (items[i].value != curr_value) {
+        if (items[i].value != curr_value || group_counts[group_idx] >= BLOCK_SIZE) {
             group_idx++;
             curr_value = items[i].value;
             unique_values[group_idx] = curr_value;
@@ -95,85 +96,77 @@ void processGroups(Item* items, int n, int*& group_counts, int*& unique_values, 
     }
 }
 
-void output(char* outFileName, int* result, int m) {
-    FILE* outfile = fopen(outFileName, "w");
+void output(char *outFileName, int result, int m) {
+    FILE *outfile = fopen(outFileName, "w");
     if (!outfile) {
         printf("Error: Cannot open output file\n");
         exit(1);
     }
 
-    int maxResult = 0;
-    for (int i = 0; i <= m; i++) {
-        maxResult = max(maxResult, result[i]);
-    }
-
-    fwrite(&maxResult, sizeof(int), 1, outfile);
+    fwrite(&result, sizeof(int), 1, outfile);
     fclose(outfile);
 }
 
-int main(int argc, char* argv[]) {
+int main(int argc, char *argv[]) {
     if (argc != 3) {
         printf("Usage: %s <input_file> <output_file>\n", argv[0]);
         return 1;
     }
-    
+
     int n, m;
-    Item* items = NULL;
-    
+    Item *items = NULL;
+
     input(argv[1], n, m, items);
-    
-    int* group_counts = NULL;
-    int* unique_values = NULL;
+
+    int *group_counts = NULL;
+    int *unique_values = NULL;
     int num_groups;
-    
+
     processGroups(items, n, group_counts, unique_values, num_groups);
-    
+
+    int const m_pad = ceil_div(m + 1, BLOCK_SIZE) * BLOCK_SIZE;
     int *d_dp_prev = NULL, *d_dp_curr = NULL;
-    cudaMalloc((void**)&d_dp_prev, (m + 1) * sizeof(int));
-    cudaMalloc((void**)&d_dp_curr, (m + 1) * sizeof(int));
-    
-    cudaMemset(d_dp_prev, 0, (m + 1) * sizeof(int));
-    cudaMemset(d_dp_curr, 0, (m + 1) * sizeof(int));
-    
-    int numBlocks = (m + BLOCK_SIZE - 1) / BLOCK_SIZE;
-    
+    cudaMalloc((void **)&d_dp_prev, m_pad * sizeof(int));
+    cudaMalloc((void **)&d_dp_curr, m_pad * sizeof(int));
+
+    // cudaMemset(d_dp_prev, 0, m_pad * sizeof(int));
+    // cudaMemset(d_dp_curr, 0, m_pad * sizeof(int));
+
+    int numBlocks = m_pad / BLOCK_SIZE;
+
     int curr_pos = 0;
+    int *d_group_weights = NULL;
+    cudaMalloc((void **)&d_group_weights, BLOCK_SIZE * sizeof(int));
+    int *group_weights = (int *)malloc(BLOCK_SIZE * sizeof(int));
     for (int g = 0; g < num_groups; g++) {
-        int* d_group_weights = NULL;
-        cudaMalloc((void**)&d_group_weights, group_counts[g] * sizeof(int));
-        
-        int* group_weights = (int*)malloc(group_counts[g] * sizeof(int));
-        for (int i = 0; i < group_counts[g]; i++) {
-            group_weights[i] = items[curr_pos + i].weight;
-        }
-        
-        cudaMemcpy(d_group_weights, group_weights, 
-                   group_counts[g] * sizeof(int), cudaMemcpyHostToDevice);
-        
-        mckp_kernel<<<numBlocks, BLOCK_SIZE>>>(d_dp_prev, d_dp_curr, m, 
-                                              d_group_weights, group_counts[g], 
-                                              unique_values[g]);
+        for (int i = 0; i < BLOCK_SIZE; i++)
+            group_weights[i] = (i < group_counts[g] ? items[curr_pos + i].weight : INF);
+
+        cudaMemcpy(d_group_weights, group_weights,
+                   BLOCK_SIZE, cudaMemcpyHostToDevice);
+
+        mckp_kernel<<<numBlocks, BLOCK_SIZE>>>(d_dp_prev, d_dp_curr,
+                                               d_group_weights, unique_values[g]);
 
         int *temp = d_dp_prev;
         d_dp_prev = d_dp_curr;
         d_dp_curr = temp;
-        
-        //cudaFree(d_group_weights);
-        //free(group_weights);
+
         curr_pos += group_counts[g];
     }
-    
-    int* result = (int*)malloc((m + 1) * sizeof(int));
-    cudaMemcpy(result, d_dp_prev, (m + 1) * sizeof(int), cudaMemcpyDeviceToHost);
-    
+    cudaFree(d_group_weights);
+    free(group_weights);
+
+    int result;
+    cudaMemcpy(&result, &d_dp_prev[m], sizeof(int), cudaMemcpyDeviceToHost);
+
     output(argv[2], result, m);
-    
+
     cudaFree(d_dp_prev);
     cudaFree(d_dp_curr);
     free(group_counts);
     free(unique_values);
     free(items);
-    free(result);
-    
+
     return 0;
 }
--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-TEST=/home/pp24/pp24s038/PP_final_project/testcases
+TEST=/home/pp24/pp24s039/PP_final_project/testcases
 
 srun -N1 -n1 --gres=gpu:1 time ./$1 $TEST/$2.in tmp.out
 
