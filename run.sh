1a33185d93c9ca398866547d8dedadbca2038f19
LittlePants <gilbert12.tw@gmail.com>
2024-12-13 13:39:56 +0800

    Add run.sh script

---
+++ run.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+TEST=/home/pp24/pp24s038/PP_final_project/testcases
+
+srun -N1 -n1 --gres=gpu:1 time ./$1 $TEST/$2.in tmp.out
+
+echo "------------------------------------"
+echo "Your answer:"
+./utilities/reader tmp.out 
+
+echo "Correct  answer:"
+./utilities/reader $TEST/$2.out
